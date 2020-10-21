b82fc561a53f9367a75093dacf72ffb88e3673dc
Birama0810 <birama.tour@gmail.com>
2020-10-21 08:47:55 +0200

    Creation d'un nouvel component 'appareil'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { App1Component } from './app1/app1.component';
+import { AppareilComponent } from './appareil/appareil.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    App1Component
+    App1Component,
+    AppareilComponent
   ],
   imports: [
     BrowserModule,
