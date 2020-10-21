376a3337cfc05346145478588960334d67ffa284
Birama0810 <birama.tour@gmail.com>
2020-10-21 21:56:07 +0200

    Creation d'un nouvel component 'appareil'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {consoleTestResultHandler} from 'tslint/lib/test';
+
+function SetTimeout(param: () => void, number: 4000) {
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-project';
+  isAuth = false;
+  appareilOne = 'Machine à laver';
+  appareilTwo = 'Frigo';
+  appareilThree = 'Ordinateur';
+  StatutOne = 'éteint';
+  StatutTwo = 'allumé';
+  StatutThree = 'éteint';
+  EtatOne = 'réconditionnée';
+  EtatTwo = 'neuf';
+  EtatThree = 'occasion';
+
+  constructor() {
+    setTimeout(
+      () => {
+        this.isAuth = true;
+      }, 4000
+    );
+  }
+  // tslint:disable-next-line:typedef
+  onAllumer(){
+    console.log(" On allume tout");
+  }
 }
